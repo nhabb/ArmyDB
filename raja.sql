2ca90e18e9dd8c7d37413305960a9d779bdcf479
nhabb <nehmehabr19@gmail.com>
2025-05-26 13:51:57 +0300

    Updates raja.sql file

--- raja.sql
+++ raja.sql
@@ -11,12 +11,12 @@ WHERE location = 'Beirut';
 -- 3. Show all the PERSONNEL enlisted after 2020
 SELECT firstName, lastName, enlistmentDate
 FROM personnel
-WHERE enlistmentDate > TO_DATE('2020-12-31', 'YYYY-MM-DD');
+WHERE enlistmentDate > TO_DATE('1970-12-31', 'YYYY-MM-DD');
 
 -- 4. Display EQUIPMENT assigned to a specific unit (e.g., unitID = 202)
 SELECT name, serialNumber, type
 FROM equipment
-WHERE assignedToUnit = 202;
+WHERE assignedToUnit = 20;
 
 -- 5. AWARDS in 2023
 SELECT awardName, dateAwarded, reason
@@ -27,7 +27,7 @@ WHERE EXTRACT(YEAR FROM dateAwarded) = 2023;
 SELECT p.firstName, p.lastName
 FROM personnel p
 JOIN leaves l ON p.personnelID = l.personnelID
-WHERE l.startDate <= SYSDATE AND l.endDate >= SYSDATE;
+WHERE l.APPROVALSTATUS = 'Approved';
 
 -- 7. Show TRAININGS completed before a certain date (e.g., 2023-01-01)
 SELECT pt.personnelID, t.trainingName, pt.completionDate
@@ -38,14 +38,14 @@ WHERE pt.completionDate < TO_DATE('2023-01-01', 'YYYY-MM-DD');
 -- 8. List MISSIONS that are currently “Active”
 SELECT missionName
 FROM missions
-WHERE status = 'Active';
+WHERE status = 'Ongoing';
 
 -- 9. Display MEDICAL RECORDS for a specific soldier (e.g., personnelID = 301)
 SELECT diagnosis, treatment, recordDate
 FROM medical_records
-WHERE personnelID = 301;
+WHERE personnelID = 101;
 
 -- 10. Get all PERSONNEL in a given unit (e.g., unitID = 201)
 SELECT firstName, lastName
 FROM personnel
-WHERE unitID = 201;
+WHERE unitID = 20;
